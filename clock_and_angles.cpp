1ab5a45521580ecb55cb520d3f4f25105a2ba15e
Abhishek Gadge <69665891+Abhishek-Gadge@users.noreply.github.com>
2020-10-11 23:03:49 +0530

    Add files via upload

---
+++ clock_and_angles.cpp
@@ -0,0 +1,31 @@
+//Clock and Angles
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //write your code here
+  int t;
+  cin>>t;
+  while(t>0)
+  {
+    int h,m,hour,minute,diff;
+    cin>>h>>m;
+    hour=(h*360)/12+(m*360)/(12*60);
+    minute=(m*360)/(60);
+    diff=abs(hour-minute);
+    if(diff>180)
+    {
+      cout<<endl<<360-diff;
+    }
+    else
+    {
+       cout<<endl<<diff;
+    }
+   
+    t--;
+  }
+  return 0;
+}
